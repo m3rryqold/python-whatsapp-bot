da7ae4adc55a1ec0dd187da6568bfa591a0f107c
Merrygold <merrymcx@gmail.com>
2022-06-25 15:32:34 +0100

    base send message template

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # python-whatsapp-bot
+
+
+sudo apt-get install python3-tk python3-dev
---
+++ src/whatsapp-sender.py
@@ -0,0 +1,4 @@
+import pywhatkit
+
+phone_number = input("Enter phone number: ")
+pywhatkit.sendwhatmsg(phone_number, "yo yo, wozop",6,34,15,True,2)
